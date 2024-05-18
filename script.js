b9a7b0c2ce214e8a8f9f23204a097a931cf5e1a1
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-05-18 15:03:54 -0700

    Add cards

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ let members = `[
   {
     "name": "Mario",
     "age": 30,
-    "description": "Mario is a mustached Italian plumber who lives in the Mushroom Kingdom. Mario also sometimes rules his own land, Mario Land. Mario and his brother, Luigi, are plumbers who originally worked in Brooklyn, New York City. Mario is known for being kind, cheerful, playful, daredevil, courageous, and headstrong and is also eager and cocky in certain occasions. ",
-    "image": "https://cdn.vox-cdn.com/thumbor/VlPF8UuUKoUHFtiebdDsQpW1zYs=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/9632107/mario.jpg"
+    "description": "Mario is a mustached Italian plumber who lives in the Mushroom Kingdom. Mario and his brother, Luigi, are plumbers who originally worked in Brooklyn, New York City. Mario is known for being kind, cheerful, playful, daredevil, courageous, and headstrong and is also eager and cocky in certain occasions. ",
+    "image": "https://pngimg.com/d/mario_PNG54.png"
   },
   {
     "name": "Luigi",
     "age": 30,
-    "description": "Luigi is Mario's younger twin brother who first appeared as a palette swap of Mario in Mario Bros. for the Arcade (and later, for the NES). He wears green instead of red and is taller and slimmer than Mario. While Mario is everyone's favorite superstar, Luigi is not nearly as well known among the citizens of the Mushroom Kingdom. Luigi is portrayed as timid, cowardice, worrisome, and self-conscious in many of his appearances, especially since Luigi's Mansion.",
-    "image": "https://i.pinimg.com/474x/65/c8/bd/65c8bd564d77603848994b97a8a7c7d1.jpg"
+    "description": "Luigi is Mario's younger twin brother. While Mario is everyone's favorite superstar, Luigi is not nearly as well known among the citizens of the Mushroom Kingdom. Luigi is portrayed as timid, cowardice, worrisome, and self-conscious in many of his appearances, especially since Luigi's Mansion.",
+    "image": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8a394efc-d4bd-466a-aa96-0c444ab1cffb/dh39znr-eee424fc-3d92-4a03-b39f-4736a173c5bf.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhhMzk0ZWZjLWQ0YmQtNDY2YS1hYTk2LTBjNDQ0YWIxY2ZmYlwvZGgzOXpuci1lZWU0MjRmYy0zZDkyLTRhMDMtYjM5Zi00NzM2YTE3M2M1YmYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.iqi0XIs6cwmjxKAiRRTWKAZYcaOwddJPyIYwoiao1Tk"
   },
   {
     "name": "Princess Peach",
     "age": 28,
     "description": "Princess Peach is the ruler of the Mushroom Kingdom, making her debut in Super Mario Bros., and the tritagonist of the Mario franchise. She is sweet, kind, optimistic, friendly, and experienced.",
-    "image": "https://i.pinimg.com/736x/0c/a3/3c/0ca33cd50d8ec0a894f9aa061b501ff0.jpg"
+    "image": "https://i.pinimg.com/originals/02/4f/5d/024f5d09af65610089fdb9ca3f5738e4.png"
   },
   {
     "name": "Bowser",
     "age": 29,
     "description": "Bowser is the main antagonist of Nintendo's Mario franchise and is the archenemy of Mario. Bowser most commonly kidnaps Princess Peach, with whom he has unrequited love for, so he can make her his queen. He is the leader of the Koopa race.",
-    "image": "https://static.wikia.nocookie.net/nonhuman-video-game-flattenings/images/6/62/Bowser.jpg/revision/latest?cb=20160203233710"
+    "image": "https://ssb.wiki.gallery/images/thumb/7/71/Bowser.png/1200px-Bowser.png"
   }
 ]`;
 
@@ -50,8 +50,30 @@ function handleFormSubmission(event) {
     form.description.value = null;
     form.age.value = null;
     form.image.value = null;
+
+    showMemberCards();
 }
 
-function showMembersCards() {
-  const cardsContainer = document.getElementById('members-cards-container');
+function showMemberCards() {
+  const cardsContainer = document.querySelector('.member-cards-container');
+  let membersArr = JSON.parse(members);
+  cardsContainer.innerHTML = ''; 
+
+  membersArr.forEach(member => {
+    const card = document.createElement('div');
+    card.classList.add('member-card');
+
+    card.innerHTML = `
+      <img src="${member.image}" alt="${member.name}">
+      <h2>${member.name}</h2>
+      <p>Age: ${member.age}</p>
+      <p>${member.description}</p>
+    `;
+
+    cardsContainer.appendChild(card);
+  });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  showMemberCards();
+});
