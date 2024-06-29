28a453c3309bf02e23a806d05416a8c8153fe834
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-06-29 15:59:56 -0700

    Set up db

--- team-builder-with-backend/server/index.js
+++ team-builder-with-backend/server/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
 import membersRouter from './routes.js';
+import dotenv from 'dotenv';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
 const port = 4000;
@@ -22,5 +28,14 @@ app.listen(port, () => {
 });
 
 app.use('/members', membersRouter);
+
+const dbStr = process.env.MONGO_DB_CONNECT_STR;
+console.log(dbStr);
+
+mongoose.connect(dbStr);
+const db = mongoose.connection; 
+
+db.on('error', (error) => console.error(error));
+db.on('open', () => console.log('Connected to Database'));
 
 export default app;
